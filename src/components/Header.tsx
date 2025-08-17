32616e02f162f452e2e220b46ce53cc232295ede
phrough <phrough@phroughs-MacBook-Air.local>
2025-08-17 11:35:39 -0500

    vite create initialization of project

---
+++ src/components/Header.tsx
@@ -0,0 +1,63 @@
+import { Link } from '@tanstack/react-router'
+
+import ClerkHeader from '../integrations/clerk/header-user.tsx'
+
+export default function Header() {
+  return (
+    <header className="p-2 flex gap-2 bg-white text-black justify-between">
+      <nav className="flex flex-row">
+        <div className="px-2 font-bold">
+          <Link to="/">Home</Link>
+        </div>
+
+        <div className="px-2 font-bold">
+          <Link to="/demo/clerk">Clerk</Link>
+        </div>
+
+        <div className="px-2 font-bold">
+          <Link to="/demo/form/simple">Simple Form</Link>
+        </div>
+
+        <div className="px-2 font-bold">
+          <Link to="/demo/form/address">Address Form</Link>
+        </div>
+
+        <div className="px-2 font-bold">
+          <Link to="/demo/start/server-funcs">Start - Server Functions</Link>
+        </div>
+
+        <div className="px-2 font-bold">
+          <Link to="/demo/start/api-request">Start - API Request</Link>
+        </div>
+
+        <div className="px-2 font-bold">
+          <Link to="/demo/trpc-todo">tRPC Todo</Link>
+        </div>
+
+        <div className="px-2 font-bold">
+          <Link to="/demo/table">TanStack Table</Link>
+        </div>
+
+        <div className="px-2 font-bold">
+          <Link to="/demo/tanstack-query">TanStack Query</Link>
+        </div>
+
+        <div className="px-2 font-bold">
+          <Link to="/demo/store">Store</Link>
+        </div>
+
+        <div className="px-2 font-bold">
+          <Link to="/demo/sentry/testing">Sentry</Link>
+        </div>
+
+        <div className="px-2 font-bold">
+          <Link to="/demo/db-chat">DB Chat</Link>
+        </div>
+      </nav>
+
+      <div>
+        <ClerkHeader />
+      </div>
+    </header>
+  )
+}
